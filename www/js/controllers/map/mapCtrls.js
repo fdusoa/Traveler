7982ad13e78fd0d6af166441a9e2dbdbef7f2619
N4A <181387406@qq.com>
2016-09-07 10:13:49 +0800

    搭建基本框架

--- www/js/controllers/map/mapCtrls.js
+++ www/js/controllers/map/mapCtrls.js
@@ -4,5 +4,7 @@
 angular.module('ctrl.map', [])
 
   .controller('MapCtrl', function($scope) {
-
+    // 百度地图API功能
+    var map = new BMap.Map("allmap");  // 创建Map实例
+    map.centerAndZoom("上海",15);      // 初始化地图,用城市名设置地图中心点
   });
