4c4ded4db3db09a75aa0814b0bc879c9583f5d0d
N4A <181387406@qq.com>
2016-10-22 23:08:04 +0800

    something

--- www/js/app.js
+++ www/js/app.js
@@ -26,18 +26,18 @@ angular.module('starter', ['ionic','starter.routers', 'starter.controllers', 'st
 .config(function($ionicConfigProvider) {
 
     //使得导航栏在不同平台中适配标准
-    $ionicConfigProvider.platform.ios.tabs.style('standard');
-    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    //$ionicConfigProvider.platform.ios.tabs.style('standard');
+    //$ionicConfigProvider.platform.ios.tabs.position('bottom');
     $ionicConfigProvider.platform.android.tabs.style('standard');
     $ionicConfigProvider.platform.android.tabs.position('standard');
 
-    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    // $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
     $ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    //$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
     $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-    $ionicConfigProvider.platform.ios.views.transition('ios');
+    //$ionicConfigProvider.platform.ios.views.transition('ios');
     $ionicConfigProvider.platform.android.views.transition('android');
 
 });
